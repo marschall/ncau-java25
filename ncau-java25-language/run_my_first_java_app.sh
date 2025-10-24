826169bdf94b1108595cef93f6b7f192fb8567e8
Philippe Marschall <philippe.marschall@netcetera.com>
2025-10-24 10:22:45 +0200

    Fix scripts

--- ncau-java25-language/run_my_first_java_app.sh
+++ ncau-java25-language/run_my_first_java_app.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-JAVA_HOME=/Users/pmarscha/Documents/dev/java/zulu25.0.47-beta-jdk25.0.0-beta.34-macosx_aarch64/zulu-25.jdk/Contents/Home
-${JAVA_HOME}/bin/java src/main/java/com/netcetera/ncau/java25/langauge/MyFirstJavaApplication.java
+JAVA_HOME=/Library/Java/JavaVirtualMachines/25.0.0/Contents/Home
+${JAVA_HOME}/bin/java src/main/java/MyFirstJavaApplication.java
