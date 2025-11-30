e6aaee5716461891fed1d56da92f88eaee47e6f8
Philippe Marschall <philippe.marschall@gmail.com>
2025-11-30 11:21:33 +0100

    update java version

--- ncau-java25-language/run_my_first_db_app.sh
+++ ncau-java25-language/run_my_first_db_app.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-JAVA_HOME=/Library/Java/JavaVirtualMachines/25.0.0/Contents/Home
+JAVA_HOME=/Library/Java/JavaVirtualMachines/25.0.1/Contents/Home
 ${JAVA_HOME}/bin/java \
   --class-path ~/.m2/repository/com/h2database/h2/2.4.240/h2-2.4.240.jar \
   src/main/java/MyFirstDatabaseApplication.java
