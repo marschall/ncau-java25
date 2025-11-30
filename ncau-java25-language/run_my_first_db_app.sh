7975c2347af939a8685539ba7018a8e1274c47ca
Philippe Marschall <philippe.marschall@gmail.com>
2025-11-30 11:00:33 +0100

    bump h2

--- ncau-java25-language/run_my_first_db_app.sh
+++ ncau-java25-language/run_my_first_db_app.sh
@@ -2,5 +2,5 @@
 
 JAVA_HOME=/Library/Java/JavaVirtualMachines/25.0.0/Contents/Home
 ${JAVA_HOME}/bin/java \
-  --class-path ~/.m2/repository/com/h2database/h2/2.3.232/h2-2.3.232.jar \
+  --class-path ~/.m2/repository/com/h2database/h2/2.4.240/h2-2.4.240.jar \
   src/main/java/MyFirstDatabaseApplication.java
